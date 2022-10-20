4ed5b792671f87a1f718029aab9ef68e67750ca2
Damien MIREMONT <damien.miremont@gmail.com>
2022-10-20 20:46:23 +0200

    essai de l'ajout de Barba.js

--- portfolio-V2/js/home.js
+++ portfolio-V2/js/home.js
@@ -1,3 +1,5 @@
+// ANIMATION PAGE ACCUEIL
+
 const titleDelay = document.querySelectorAll('h1, span');
 const imageLottie = document.querySelectorAll('lottie-player');
 const myName = document.querySelectorAll('h2');
@@ -9,9 +11,10 @@ window.addEventListener('load', () =>{
 
     delay
     .staggerFrom(titleDelay, 1, {x: -50, opacity: 0, ease: "power2.out"}, 0.3)
-    .staggerFrom(imageLottie, 0.8, {scale: 0, ease: "power2.out"},'-=2')
+    .staggerFrom(imageLottie, 1, {scale: 0, rotation: 360, ease: "power2.out"},'-=2')
     .from(myName, 1, {width:0, ease: "power2.out"}, 0.3, '-=2')
     .staggerFrom(anchre, 1, {y: -200, ease: "power2.out"}, 0.3, '-=1');
 
     delay.play();
-})
+});
+
